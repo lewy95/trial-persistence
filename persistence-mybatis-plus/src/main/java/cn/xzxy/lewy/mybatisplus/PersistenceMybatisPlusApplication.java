b4739a9d80d25150f03e51274c220ee04dec34e1
lewy95 <171133755@qq.com>
2020-06-01 11:22:36 +0800

    add mybatis-plus support

---
+++ persistence-mybatis-plus/src/main/java/cn/xzxy/lewy/mybatisplus/PersistenceMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package cn.xzxy.lewy.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceMybatisPlusApplication.class, args);
+    }
+
+}
