23bf4820208cbbcf940ca2dafdabf6bf19465f4a
lewy95 <171133755@qq.com>
2020-07-14 14:05:24 +0800

    add elasticsearch module

---
+++ persistence-elasticsearch/src/main/java/cn/xzxy/lewy/elasticsearch/PersistenceElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package cn.xzxy.lewy.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceElasticsearchApplication.class, args);
+    }
+
+}
