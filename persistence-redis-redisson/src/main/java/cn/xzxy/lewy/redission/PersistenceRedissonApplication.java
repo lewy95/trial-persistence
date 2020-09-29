18e3c29a92b4e6514be309ad71ff79de8cd8c7af
lewy95 <171133755@qq.com>
2020-09-29 18:02:55 +0800

    fix/to: add redisson module

---
+++ persistence-redis-redisson/src/main/java/cn/xzxy/lewy/redission/PersistenceRedissonApplication.java
@@ -0,0 +1,13 @@
+package cn.xzxy.lewy.redission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceRedissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceRedissonApplication.class, args);
+    }
+
+}
