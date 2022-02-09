b6def398ae8c23b33599941752e8d3610d991005
yangjt <yangjt@yoozoo.com>
2022-02-09 12:18:56 +0800

    fix/to: change project name

--- persistence-mybatis/src/main/java/cn/xzxy/lewy/mybatis/controller/CountryController.java
+++ persistence-mybatis/src/main/java/cn/xzxy/lewy/mybatis/controller/CountryController.java
@@ -2,6 +2,7 @@ package cn.xzxy.lewy.mybatis.controller;
 
 import cn.xzxy.lewy.mybatis.common.model.JsonResponseEntity;
 import cn.xzxy.lewy.mybatis.dto.CountryDetailReq;
+import cn.xzxy.lewy.mybatis.pojo.Country;
 import cn.xzxy.lewy.mybatis.service.CountryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,9 +18,15 @@ public class CountryController {
     @Resource
     private CountryService countryService;
 
-    @PostMapping("/detail")
+    @PostMapping("/detail1")
     @ApiOperation(value = "根据ID查询城市", notes = "根据ID查询城市")
-    public JsonResponseEntity getCountry(@RequestBody CountryDetailReq countryDetailReq) {
+    public JsonResponseEntity<Country> getCountry(@RequestBody CountryDetailReq countryDetailReq) {
+        return JsonResponseEntity.buildOK(countryService.getCountry(countryDetailReq.getCountryId()));
+    }
+
+    @PostMapping("/detail2")
+    @ApiOperation(value = "根据ID查询城市", notes = "根据ID查询城市")
+    public JsonResponseEntity<Country> getCountry2(@RequestBody CountryDetailReq countryDetailReq) {
         return JsonResponseEntity.buildOK(countryService.getCountry(countryDetailReq.getCountryId()));
     }
 }
